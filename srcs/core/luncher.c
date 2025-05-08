cce1e5e423828478d615b507ec6258691331eca3
Walid Imam <wimam@e2r10p1.1337.ma>
2025-05-08 11:41:55 +0100

    1 - fix a chiled segfault because readline return '\0' when giving empty prompt

--- srcs/core/luncher.c
+++ srcs/core/luncher.c
@@ -6,7 +6,7 @@
 /*   By: wimam <walidimam69gmail.com>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/07 11:27:59 by wimam             #+#    #+#             */
-/*   Updated: 2025/05/08 10:23:14 by wimam            ###   ########.fr       */
+/*   Updated: 2025/05/08 11:41:32 by wimam            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,9 +30,12 @@ void	ft_luncher(t_ms *ms)
 {
 	while (ft_redline(ms) == TRUE)
 	{
-		ft_parse(ms);
-		init_cmd(ms);
-		ft_exe(ms);
-		reset_prompt(ms);
+		if (*ms->input)
+		{
+			ft_parse(ms);
+			init_cmd(ms);
+			ft_exe(ms);
+			reset_prompt(ms);
+		}
 	}
 }
