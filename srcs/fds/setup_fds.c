acb72867540ec5a4fd54ffa2bc3a060cd1f66370
Walid Imam <wimam@e2r9p2.1337.ma>
2025-05-10 12:01:26 +0100

    fix norms

--- srcs/fds/setup_fds.c
+++ srcs/fds/setup_fds.c
@@ -6,7 +6,7 @@
 /*   By: wimam <walidimam69gmail.com>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/09 11:26:15 by wimam             #+#    #+#             */
-/*   Updated: 2025/05/09 13:46:49 by wimam            ###   ########.fr       */
+/*   Updated: 2025/05/10 12:00:45 by wimam            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,11 +29,14 @@ void	reset_fds(t_ms *ms)
 
 void	setup_outfd(t_ms *ms)
 {
+	int	*fd;
+
+	fd = &ms->fds.oufd;
 	if (ms->fds.append_flag == TRUE)
-		ms->fds.oufd = open(ms->parse.oustr, O_WRONLY | O_CREAT | O_APPEND, 0644);
+		*fd = open(ms->parse.oustr, O_WRONLY | O_CREAT | O_APPEND, 0644);
 	else
-		ms->fds.oufd = open(ms->parse.oustr, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-	if (ms->fds.oufd < 0)
+		*fd = open(ms->parse.oustr, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	if (*fd < 0)
 		return (err_msg(ERR_OPEN_F));
 }
 
