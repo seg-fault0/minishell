835b127c1e743e81b1ce4dce3a589f0e1f2a8f00
Walid Imam <wimam@e2r9p2.1337.ma>
2025-05-10 14:43:40 +0100

    here_doc is kinda working

--- srcs/fds/here_doc.c
+++ srcs/fds/here_doc.c
@@ -6,7 +6,7 @@
 /*   By: wimam <walidimam69gmail.com>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/10 14:08:30 by wimam             #+#    #+#             */
-/*   Updated: 2025/05/10 14:23:37 by wimam            ###   ########.fr       */
+/*   Updated: 2025/05/10 14:39:56 by wimam            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,14 +25,14 @@ void	here_doc(t_ms *ms)
 	line = get_next_line(STDIN);
 	if (!line)
 		return (close_pipe(pfd));
-	while (line && ft_strcmp(line, delimiter) == FALSE)
+	while (line && ft_memcmp(line, delimiter, ft_strlen(delimiter)) != 0)
 	{
 		write (pfd[1], line, ft_strlen(line));
 		free(line);
 		ft_putstr_fd(HERE_DOC, STDOUT);
 		line = get_next_line(STDIN);
 	}
-	get_next_line(GNL_FREE);
+	//get_next_line(GNL_FREE);
 	close(pfd[1]);
 	ms->fds.infd = pfd[0];
 }
--- srcs/fds/setup_fds.c
+++ srcs/fds/setup_fds.c
@@ -6,7 +6,7 @@
 /*   By: wimam <walidimam69gmail.com>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/09 11:26:15 by wimam             #+#    #+#             */
-/*   Updated: 2025/05/10 13:54:34 by wimam            ###   ########.fr       */
+/*   Updated: 2025/05/10 14:40:52 by wimam            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,16 +20,18 @@ void	reset_fds(t_ms *ms)
 		close (ms->fds.oufd);
 	free(ms->parse.instr);
 	free(ms->parse.oustr);
+	free(ms->parse.delimiter);
 	ms->parse.instr = NULL;
 	ms->parse.oustr = NULL;
 	ms->fds.infd = STDIN;
 	ms->fds.oufd = STDOUT;
 	ms->fds.append_flag = FALSE;
+	ms->parse.delimiter = NULL;
 }
 
 void	setup_fds(t_ms *ms)
 {
-	if (ms->parse.instr)
+	if (ms->parse.instr || ms->parse.delimiter)
 		setup_infd(ms);
 	if (ms->parse.oustr)
 		setup_outfd(ms);
