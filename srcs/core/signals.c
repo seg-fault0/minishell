4cd08ea5ca037278206c600162ebcf129facf696
Walid Imam <wimam@e2r9p2.1337.ma>
2025-05-15 12:02:04 +0100

    fix signals (ctr^C printing)

--- srcs/core/signals.c
+++ srcs/core/signals.c
@@ -6,11 +6,20 @@
 /*   By: wimam <walidimam69gmail.com>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/10 15:35:48 by wimam             #+#    #+#             */
-/*   Updated: 2025/05/10 16:19:21 by wimam            ###   ########.fr       */
+/*   Updated: 2025/05/15 12:01:25 by wimam            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+void	disable_echoctl(void)
+{
+	struct termios	term;
+
+	tcgetattr(STDIN_FILENO, &term);
+	term.c_lflag &= ~ECHOCTL;
+	tcsetattr(STDIN_FILENO, TCSANOW, &term);
+}
 
 void	handle_sigint(int sig)
 {
@@ -29,6 +38,7 @@ void	handle_sigquit(int sig)
 
 void	setup_signals(void)
 {
+	disable_echoctl();
 	signal(SIGINT, handle_sigint);
 	signal(SIGQUIT, handle_sigquit);
 }
