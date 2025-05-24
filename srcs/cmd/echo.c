2b23c8b6f5be22db6f6fb3e63791915006384df4
Walid Imam <wimam@e2r9p2.1337.ma>
2025-05-24 11:11:31 +0100

    add flag_handler function for echo

--- srcs/cmd/echo.c
+++ srcs/cmd/echo.c
@@ -6,17 +6,24 @@
 /*   By: wimam <walidimam69gmail.com>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/24 11:01:09 by wimam             #+#    #+#             */
-/*   Updated: 2025/05/24 11:01:27 by wimam            ###   ########.fr       */
+/*   Updated: 2025/05/24 11:11:00 by wimam            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+static void	flag_handler(t_ms *ms, char *str, int *i)
+{
+	if (ft_memcmp(str, "$?", 2) == 0)
+		ft_putnbr(ms->cmd.last_exit_code);
+	*i += 1;
+}
+
 void	ft_echo(t_ms *ms)
 {
 	char	**cmd_args;
-	int	i;
-	int	j;
+	int		i;
+	int		j;
 
 	cmd_args = ms->cmd.cmd[ms->cmd.counter];
 	i = 0;
@@ -26,10 +33,7 @@ void	ft_echo(t_ms *ms)
 		while (cmd_args[i][++j])
 		{
 			if (ft_memcmp(&cmd_args[i][j], "$?", 2) == 0)
-			{
-				ft_putnbr(ms->cmd.last_exit_code);
-				j++;
-			}
+				flag_handler(ms, &cmd_args[i][j], &j);
 			else
 				write(STDOUT, &cmd_args[i][j], 1);
 		}
