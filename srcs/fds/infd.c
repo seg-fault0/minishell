d52c6fd662770b50d177d8b13fadf9ec1261cf00
Walid Imam <wimam@e2r9p2.1337.ma>
2025-05-26 13:13:36 +0100

    protect open in infd

--- srcs/fds/infd.c
+++ srcs/fds/infd.c
@@ -6,7 +6,7 @@
 /*   By: wimam <walidimam69gmail.com>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/17 13:10:28 by wimam             #+#    #+#             */
-/*   Updated: 2025/05/26 10:41:10 by wimam            ###   ########.fr       */
+/*   Updated: 2025/05/26 13:12:26 by wimam            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,10 @@ void	in_fd_opener(t_ms *ms)
 		while (ms->parse.infiles[i] && ms->parse.infiles[i][j])
 			j++;
 		if (j > 0)
+		{
 			ms->fd.in[i] = open(ms->parse.infiles[i][j - 1], O_RDONLY);
+			if (ms->fd.in[i] == -1)
+				err_msg(ERR_OPEN_F);
+		}
 	}
 }
