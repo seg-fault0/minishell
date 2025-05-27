a9f815d3f5901d1c68b6e7cb846b4df545d5bbc4
Walid Imam <wimam@e2r9p2.1337.ma>
2025-05-27 12:03:41 +0100

    better export

--- srcs/builtin/export.c
+++ srcs/builtin/export.c
@@ -6,7 +6,7 @@
 /*   By: wimam <walidimam69gmail.com>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/25 16:43:55 by wimam             #+#    #+#             */
-/*   Updated: 2025/05/26 12:02:56 by wimam            ###   ########.fr       */
+/*   Updated: 2025/05/27 12:00:54 by wimam            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,17 @@
 
 static BOOL	env_synthax(char *str)
 {
-	if (str[0] == '\0' || str[0] == '=' || is_digit(str[0]))
+	int	i;
+
+	if (str[0] == '\0' || str[0] == '=')
 		return (TRUE);
+	i = 0;
+	while (str[i] && str[i] != '=')
+	{
+		if (is_digit(str[0]) || str[i] != '-')
+			return (TRUE);
+		i++;
+	}	
 	return (FALSE);
 }
 
@@ -34,7 +43,7 @@ void	set_env(t_ms *ms)
 			ft_putstr_fd("bash: export: not a valid identifier\n", STDERR);
 			ms->cmd.cur_exit_code = 1;
 		}
-		else if (ms->cmd.counter == 0)
+		else if (ms->cmd.max_counter == 1)
 		{
 			tmp_arr = ms->env;
 			ms->env = add_to_arr(tmp_arr, ms->cmd.cmd[counter][i]);
