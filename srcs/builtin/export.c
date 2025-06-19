4993b7e0313c73eeb68630f3b34e0c4e3e0d9d4f
Walid Imam <wimam@e2r9p11.1337.ma>
2025-06-19 17:06:44 +0100

    better export

--- srcs/builtin/export.c
+++ srcs/builtin/export.c
@@ -6,7 +6,7 @@
 /*   By: wimam <walidimam69gmail.com>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/25 16:43:55 by wimam             #+#    #+#             */
-/*   Updated: 2025/06/17 10:02:46 by wimam            ###   ########.fr       */
+/*   Updated: 2025/06/19 17:05:56 by wimam            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,6 +28,31 @@ static BOOL	env_synthax(char *str)
 	return (FALSE);
 }
 
+void	env_var_checker(t_ms *ms, char *str)
+{
+	char	*tmp_envar;
+	char	**tmp_arr;
+	int		len;
+	
+	tmp_envar = ft_strdup(str);
+	len = 0;
+	while (str[len] && str[len] != '=')
+		len++;
+	tmp_envar[len] = '\0';
+	if (get_env(ms->env, tmp_envar) == NULL)
+	{
+		ft_putstr_fd("minishell: export: not a valid identifier\n", STDERR);
+		return (free(tmp_envar));
+	}
+	else
+	{
+		tmp_arr = ms->env;
+		ms->env = extract_from_arr(tmp_arr, tmp_envar);
+		free2(tmp_arr, HEAP);
+		free(tmp_envar);
+	}
+}
+
 void	set_env(t_ms *ms)
 {
 	char	**tmp_arr;
@@ -39,11 +64,8 @@ void	set_env(t_ms *ms)
 	while (ms->cmd.cmd[counter][++i])
 	{
 		if (env_synthax(ms->cmd.cmd[counter][i]) == TRUE)
-		{
-			ft_putstr_fd("minishell: export: not a valid identifier\n", STDERR);
 			ms->cmd.cur_exit_code = 1;
-		}
-		else
+		else if (ft_strstr(ms->cmd.cmd[counter][i], "="))
 		{
 			tmp_arr = ms->env;
 			ms->env = add_to_arr(tmp_arr, ms->cmd.cmd[counter][i]);
