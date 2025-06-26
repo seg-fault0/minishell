c961ddfefc3884aa17b3d51748f18ba99330de96
Walid Imam <wimam@e2r7p2.1337.ma>
2025-06-26 16:34:08 +0100

    print exit when exiting and ^c when sigquit

--- srcs/signals/ms_signals.c
+++ srcs/signals/ms_signals.c
@@ -1,25 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   signals.c                                          :+:      :+:    :+:   */
+/*   ms_signals.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: wimam <walidimam69gmail.com>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/16 16:09:32 by wimam             #+#    #+#             */
-/*   Updated: 2025/06/24 16:46:14 by wimam            ###   ########.fr       */
+/*   Updated: 2025/06/26 16:32:16 by wimam            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
-
-void	disable_echoctl(void)
-{
-	struct termios	term;
-
-	tcgetattr(STDIN_FILENO, &term);
-	term.c_lflag &= ~ECHOCTL;
-	tcsetattr(STDIN_FILENO, TCSANOW, &term);
-}
 
 static void	handle_sigint(int sig)
 {
@@ -38,7 +29,6 @@ static void	handle_sigquit(int sig)
 
 void	minishell_signals(void)
 {
-	disable_echoctl();
 	signal(SIGINT, handle_sigint);
 	signal(SIGQUIT, handle_sigquit);
 }
