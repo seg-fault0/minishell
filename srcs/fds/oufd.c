225fe2f5382f8b0221ff888d6539f3265c6ca1b9
Walid Imam <wimam@e2r9p4.1337.ma>
2025-06-27 01:40:10 +0100

    fix (ls > /dev/stdout | pwd)

--- srcs/fds/oufd.c
+++ srcs/fds/oufd.c
@@ -6,11 +6,22 @@
 /*   By: wimam <walidimam69gmail.com>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/17 09:56:57 by wimam             #+#    #+#             */
-/*   Updated: 2025/06/26 22:54:22 by wimam            ###   ########.fr       */
+/*   Updated: 2025/06/27 01:39:13 by wimam            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+int	get_fd(t_ms *ms, char *filename, int i, int j)
+{
+	int	fd;
+
+	if ((ms->fd.append[i] >> j) & 1)
+		fd = open(filename, O_WRONLY | O_CREAT | O_APPEND, 0644);
+	else
+		fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	return (fd);
+}
 
 void	handle_outfile(t_ms *ms, char **arr, int i)
 {
@@ -22,21 +33,21 @@ void	handle_outfile(t_ms *ms, char **arr, int i)
 	while (arr && arr[++j])
 	{
 		filename = arr[j];
-		if ((ms->fd.append[i] >> j) & 1)
-			fd = open(filename, O_WRONLY | O_CREAT | O_APPEND, 0644);
-		else
-			fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-		if (fd == -1)
+		if (ft_strcmp(filename, "/dev/stdout") == FALSE)
 		{
-			ft_putstr_fd("permission denied\n", STDERR);
-			ms->cmd.cur_exit_code = 1;
-			ms->fd.out[i] = fd;
-			break ;
+			fd = get_fd(ms, filename, i, j);
+			if (fd == -1)
+			{
+				ft_putstr_fd("permission denied\n", STDERR);
+				ms->cmd.cur_exit_code = 1;
+				ms->fd.out[i] = fd;
+				break ;
+			}
+			if (arr[j + 1] == NULL || stdout_file_checker(&arr[j + 1]) == TRUE)
+				return (ms->fd.out[i] = fd, (void) 0);
+			else
+				ft_close(fd);
 		}
-		if (arr[j + 1] == NULL || stdout_file_checker(&arr[j + 1]) == TRUE)
-			return (ms->fd.out[i] = fd, (void) 0);
-		else
-			ft_close(fd);
 	}
 }
 
