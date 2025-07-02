8c8eea0ce8f22fa81c1db315d35c000ad13a8a2b
GRIRZouhair <zouhairgrir937@gmail.com>
2025-07-02 18:41:38 +0100

    handling of $ followed by digits in variable expansion

--- srcs/parsing/expand_vars.c
+++ srcs/parsing/expand_vars.c
@@ -6,7 +6,7 @@
 /*   By: zogrir <zogrir@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/18 10:56:01 by zogrir            #+#    #+#             */
-/*   Updated: 2025/06/25 10:42:15 by zogrir           ###   ########.fr       */
+/*   Updated: 2025/07/02 18:39:39 by zogrir           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,16 +37,25 @@ static char	*expand_generic(t_ms *ms, char *str, BOOL skip_quotes)
 	(1) && (i = -1, start = 0, result = NULL);
 	while (str[++i])
 	{
-		if ((ft_isalnum(str[i + 1]) || str[i + 1] == '?' || str[i + 1] == '_')
-			&& str[i] == '$' && (!skip_quotes || !is_in_single_quotes(str, i)))
+		if (str[i] == '$' && (!skip_quotes || !is_in_single_quotes(str, i)))
 		{
-			result = append_text_before_dollar(result, str, start, i);
-			expanded = extract_and_expand_var(ms, str, &i);
-			tmp = result;
-			result = ft_strjoin(result, expanded);
-			free(expanded);
-			free(tmp);
-			start = i + 1;
+			if (is_digit(str[i + 1]))
+			{
+				result = append_text_before_dollar(result, str, start, i);
+				i++;
+				start = i + 1;
+				continue;
+			}
+			if ((ft_isalnum(str[i + 1]) || str[i + 1] == '?' || str[i + 1] == '_'))
+			{
+				result = append_text_before_dollar(result, str, start, i);
+				expanded = extract_and_expand_var(ms, str, &i);
+				tmp = result;
+				result = ft_strjoin(result, expanded);
+				free(expanded);
+				free(tmp);
+				start = i + 1;
+			}
 		}
 	}
 	if (start < i)
